0c0c208a41388775d9638c68e893e0e88d4876fe
Kamaldinov Dinar <dkamaldinov@simtechdev.org>
2025-08-27 16:17:44 +0400

    CD-5603 : Adding a Show result button for filters

---
+++ app/addons/sd_home_filters/schemas/block_manager/blocks.post.php
@@ -0,0 +1,20 @@
+<?php
+/****************************************************************************
+ *                                                                          *
+ *   © Simtech Development Ltd.                                             *
+ *                                                                          *
+ * This  is  commercial  software,  only  users  who have purchased a valid *
+ * license  and  accept  to the terms of the  License Agreement can install *
+ * and use this program.                                                    *
+ ***************************************************************************/
+
+defined('BOOTSTRAP') or die('Access denied');
+
+/** @var array $schema */
+
+$schema['product_filters_home']['settings']['sd_apply_button'] = [
+    'type'          => 'checkbox',
+    'default_value' => 'N',
+];
+
+return $schema;
---
+++ design/themes/abt__unitheme2/templates/addons/sd_home_filters/hooks/index/styles.post.tpl
@@ -0,0 +1,1 @@
+{style src="addons/sd_home_filters/styles.less"}
