48416898d87fad55b36ec3605041723f75b7cf7b
Alexander Chuprun <achuprun@simtechdev.org>
2025-10-14 15:21:32 +0400

    CD-5617 : adding design addon

---
+++ app/addons/sd_design_changes/schemas/block_manager/blocks.post.php
@@ -0,0 +1,18 @@
+<?php
+/***************************************************************************
+ *                                                                          *
+ *   © Simtech Development Ltd.                                             *
+ *                                                                          *
+ * This  is  commercial  software,  only  users  who have purchased a valid *
+ * license  and  accept  to the terms of the  License Agreement can install *
+ * and use this program.                                                    *
+ ***************************************************************************/
+
+use Tygh\Registry;
+use Tygh\Enum\YesNo;
+use Tygh\Enum\ObjectStatuses;
+
+defined('BOOTSTRAP') or die('Access denied');
+
+
+return $schema;
