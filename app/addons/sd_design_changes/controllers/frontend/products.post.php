d451a055b13e8509156624c3a9fa9cc9154e231c
Alexander Uginov <auginov@simtechdev.org>
2025-10-30 18:04:39 +0400

    CD-5616 : Add vendor logo

--- app/addons/sd_design_changes/controllers/frontend/products.post.php
+++ app/addons/sd_design_changes/controllers/frontend/products.post.php
@@ -25,7 +25,9 @@ if ($mode == 'view' || $mode == 'quick_view') {
 
     $product = $view->getTemplateVars('product');
     if (!empty($product['company_id'])) {
-        $view->assign('company_data', fn_get_company_data($product['company_id']));
+        $view->assign('company_data', array_merge(fn_get_company_data($product['company_id']), [
+            'logos' => fn_get_logos($product['company_id']),
+        ]));
 
         if ($field_id = Registry::get('addons.sd_design_changes.metro_profile_field_id')) {
             $view->assign('company_profile_fields', fn_get_profile_fields(ProfileFieldLocations::ADMIN_FIELDS, [], CART_LANGUAGE, [
