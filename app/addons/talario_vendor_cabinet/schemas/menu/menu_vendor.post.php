82815fab2a66d5cd0fe9af583c583c53b2e9ef9b
vasilpo <vasilpogonin@gmail.com>
2026-08-28 01:24:50 +0300

    feat: expand partner cabinet sections

--- app/addons/talario_vendor_cabinet/schemas/menu/menu_vendor.post.php
+++ app/addons/talario_vendor_cabinet/schemas/menu/menu_vendor.post.php
@@ -4,7 +4,8 @@ use Tygh\Registry;
 
 defined('BOOTSTRAP') or die('Access denied');
 
-if (!Registry::get('runtime.company_id')) {
+$company_id = (int) Registry::get('runtime.company_id');
+if (!$company_id) {
     return $schema;
 }
 
@@ -28,26 +29,58 @@ $schema['central'] = [
     ],
     'seller_tools' => [
         'position'  => 300,
-        'title'     => __('talario_vendor_cabinet.centers'),
+        'title'     => __('talario_vendor_cabinet.center'),
         'href'      => 'talario_locations.manage',
         'alt'       => 'talario_locations.update',
         'icon'      => 'map_marker',
         'items'     => [],
         'is_custom' => true,
     ],
-    'settings' => [
+    'talario_messages' => [
         'position'  => 400,
+        'title'     => __('talario_vendor_cabinet.messages'),
+        'href'      => 'talario_messages.manage',
+        'icon'      => 'comments',
+        'items'     => [],
+        'is_custom' => true,
+    ],
+    'talario_notifications' => [
+        'position'  => 500,
+        'title'     => __('talario_vendor_cabinet.notifications'),
+        'href'      => 'talario_notifications.manage',
+        'icon'      => 'bell',
+        'items'     => [],
+        'is_custom' => true,
+    ],
+    'talario_finance' => [
+        'position'  => 600,
+        'title'     => __('talario_vendor_cabinet.finance'),
+        'href'      => 'companies.balance',
+        'icon'      => 'money',
+        'items'     => [],
+        'is_custom' => true,
+    ],
+    'settings' => [
+        'position'  => 700,
         'title'     => __('talario_vendor_cabinet.profile'),
-        'href'      => 'profiles.update',
+        'href'      => 'companies.update?company_id=' . $company_id,
         'icon'      => 'user',
         'items'     => [],
         'is_custom' => true,
     ],
+    'talario_documents' => [
+        'position'  => 800,
+        'title'     => __('talario_vendor_cabinet.documents'),
+        'href'      => 'talario_documents.manage',
+        'icon'      => 'file_text',
+        'items'     => [],
+        'is_custom' => true,
+    ],
     'content' => [
-        'position'  => 500,
+        'position'  => 900,
         'title'     => __('talario_vendor_cabinet.support'),
         'href'      => 'talario_support.manage',
-        'icon'      => 'comments',
+        'icon'      => 'life_ring',
         'items'     => [],
         'is_custom' => true,
     ],
