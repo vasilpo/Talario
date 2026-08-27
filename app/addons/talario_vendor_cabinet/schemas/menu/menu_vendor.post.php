b5e02877df3690bf120c5968af382d0dded34e11
vasilpo <vasilpogonin@gmail.com>
2026-08-28 00:08:31 +0300

    Finalize flat vendor navigation

--- app/addons/talario_vendor_cabinet/schemas/menu/menu_vendor.post.php
+++ app/addons/talario_vendor_cabinet/schemas/menu/menu_vendor.post.php
@@ -9,44 +9,47 @@ if (!Registry::get('runtime.company_id')) {
 }
 
 $schema['central'] = [
+    'products' => [
+        'position'  => 100,
+        'title'     => __('talario_vendor_cabinet.classes'),
+        'href'      => 'talario_classes.manage',
+        'alt'       => 'products.add,products.update',
+        'icon'      => 'tag',
+        'items'     => [],
+        'is_custom' => true,
+    ],
+    'orders' => [
+        'position'  => 200,
+        'title'     => __('talario_vendor_cabinet.bookings'),
+        'href'      => 'ec_table_booking_system.booked_orders',
+        'icon'      => 'calendar',
+        'items'     => [],
+        'is_custom' => true,
+    ],
     'seller_tools' => [
-        'position' => 100,
-        'title'    => 'Talario',
-        'icon'     => 'home',
-        'items'    => [
-            'talario_home' => [
-                'href'     => 'talario_dashboard.manage',
-                'position' => 100,
-                'title'    => __('talario_vendor_cabinet.home'),
-            ],
-            'talario_classes' => [
-                'href'     => 'talario_classes.manage',
-                'alt'      => 'products.add,products.update',
-                'position' => 200,
-                'title'    => __('talario_vendor_cabinet.classes'),
-            ],
-            'talario_bookings' => [
-                'href'     => 'ec_table_booking_system.booked_orders',
-                'position' => 300,
-                'title'    => __('talario_vendor_cabinet.bookings'),
-            ],
-            'talario_centers' => [
-                'href'     => 'talario_locations.manage',
-                'alt'      => 'talario_locations.update',
-                'position' => 400,
-                'title'    => __('talario_vendor_cabinet.centers'),
-            ],
-            'talario_profile' => [
-                'href'     => 'profiles.update',
-                'position' => 500,
-                'title'    => __('talario_vendor_cabinet.profile'),
-            ],
-            'talario_support' => [
-                'href'     => 'vendor_communication.threads',
-                'position' => 600,
-                'title'    => __('talario_vendor_cabinet.support'),
-            ],
-        ],
+        'position'  => 300,
+        'title'     => __('talario_vendor_cabinet.centers'),
+        'href'      => 'talario_locations.manage',
+        'alt'       => 'talario_locations.update',
+        'icon'      => 'map_marker',
+        'items'     => [],
+        'is_custom' => true,
+    ],
+    'settings' => [
+        'position'  => 400,
+        'title'     => __('talario_vendor_cabinet.profile'),
+        'href'      => 'profiles.update',
+        'icon'      => 'user',
+        'items'     => [],
+        'is_custom' => true,
+    ],
+    'content' => [
+        'position'  => 500,
+        'title'     => __('talario_vendor_cabinet.support'),
+        'href'      => 'talario_support.manage',
+        'icon'      => 'comments',
+        'items'     => [],
+        'is_custom' => true,
     ],
 ];
 
