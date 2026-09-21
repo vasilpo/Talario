38b0cf962203e52bd2d643942af323c56b53cffc
vasilpo <vasilpogonin@gmail.com>
2026-09-21 17:43:35 +0300

    PART-SYNC: trust dev apply route

--- app/addons/talario_analytics/schemas/permissions/trusted_controllers.post.php
+++ app/addons/talario_analytics/schemas/permissions/trusted_controllers.post.php
@@ -5,6 +5,7 @@ defined('BOOTSTRAP') or die('Access denied');
 $schema['talario_analytics'] = [
     'allow' => [
         'catalog' => true,
+        'catalog_apply' => true,
     ],
     'default_allow' => false,
     'areas' => ['C'],
