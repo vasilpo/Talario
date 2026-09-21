a92add07000e6d3ff692daf1ee356f853e81dd3c
vasilpo <vasilpogonin@gmail.com>
2026-09-21 17:57:53 +0300

    PART-SYNC: remove public write route

--- app/addons/talario_analytics/schemas/permissions/trusted_controllers.post.php
+++ app/addons/talario_analytics/schemas/permissions/trusted_controllers.post.php
@@ -5,7 +5,6 @@ defined('BOOTSTRAP') or die('Access denied');
 $schema['talario_analytics'] = [
     'allow' => [
         'catalog' => true,
-        'catalog_apply' => true,
     ],
     'default_allow' => false,
     'areas' => ['C'],
