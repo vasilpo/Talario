bcd1a9187767b08655bea7c6aa9c6dc3c75bbbcc
vasilpo <vasilpogonin@gmail.com>
2026-09-21 21:09:02 +0300

    feat(crm): allow guarded customer360 controller mode

--- app/addons/talario_analytics/schemas/permissions/trusted_controllers.post.php
+++ app/addons/talario_analytics/schemas/permissions/trusted_controllers.post.php
@@ -5,6 +5,7 @@ defined('BOOTSTRAP') or die('Access denied');
 $schema['talario_analytics'] = [
     'allow' => [
         'catalog' => true,
+        'customer360' => true,
     ],
     'default_allow' => false,
     'areas' => ['C'],
