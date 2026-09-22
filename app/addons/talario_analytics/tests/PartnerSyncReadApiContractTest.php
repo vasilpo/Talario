570db434181b8f8aac374f0b37f8c55d709a422e
vasilpo <vasilpogonin@gmail.com>
2026-09-23 02:57:28 +0300

    test: require runner uid binding

--- app/addons/talario_analytics/tests/PartnerSyncReadApiContractTest.php
+++ app/addons/talario_analytics/tests/PartnerSyncReadApiContractTest.php
@@ -127,6 +127,8 @@ final class PartnerSyncReadApiContractTest extends TestCase
         self::assertStringContainsString('PARTNER_SYNC_RUNNER_TRUST_FAILED', $this->cli_runner);
         self::assertStringContainsString("@fopen(__FILE__, 'rb')", $this->cli_runner);
         self::assertStringContainsString('@fstat($runner_handle)', $this->cli_runner);
+        self::assertStringContainsString("function_exists('posix_geteuid')", $this->cli_runner);
+        self::assertStringContainsString('PARTNER_SYNC_RUNNER_OWNER_MISMATCH', $this->cli_runner);
         self::assertStringNotContainsString('@lstat(__FILE__)', $this->cli_runner);
         self::assertStringNotContainsString('is_link(__FILE__)', $this->cli_runner);
         self::assertStringContainsString('$trusted_root_uids = [0, (int) $script_stat[\'uid\']];', $this->cli_runner);
@@ -196,7 +198,7 @@ final class PartnerSyncReadApiContractTest extends TestCase
         self::assertStringContainsString('trusted PHP binary owner mismatch', $this->dev_dispatcher);
         self::assertStringContainsString('trusted PHP binary is group/world writable', $this->dev_dispatcher);
         self::assertStringContainsString('mktemp "$STATE_DIR/runner.XXXXXX.php"', $this->dev_dispatcher);
-        self::assertStringContainsString('EXPECTED_RUNNER_SHA256="d9c5b8e4803cc5cb3057954f9e3aeefacb3e13e89e0679253581a511e042007f"', $this->dev_dispatcher);
+        self::assertStringContainsString('EXPECTED_RUNNER_SHA256="392331d0200d9372433a9b86871ee777ef9f75c7731a1a4294d62a148ca41b08"', $this->dev_dispatcher);
         self::assertStringContainsString('partner sync CLI runner is not allowlisted', $this->dev_dispatcher);
         $runner_hash = hash('sha256', $this->cli_runner);
         self::assertStringContainsString('EXPECTED_RUNNER_SHA256="' . $runner_hash . '"', $this->dev_dispatcher);
