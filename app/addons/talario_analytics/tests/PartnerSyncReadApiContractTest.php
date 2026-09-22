2b18b03c57c96721d7956b948e42d8c557c67cc9
vasilpo <vasilpogonin@gmail.com>
2026-09-23 02:39:48 +0300

    test: cover shared-hosting root permission guard

--- app/addons/talario_analytics/tests/PartnerSyncReadApiContractTest.php
+++ app/addons/talario_analytics/tests/PartnerSyncReadApiContractTest.php
@@ -124,6 +124,7 @@ final class PartnerSyncReadApiContractTest extends TestCase
         self::assertStringContainsString("'/talario.ru/public_html/dev_copy'", $this->cli_runner);
         self::assertStringContainsString('PARTNER_SYNC_ROOT_OWNER_MISMATCH', $this->cli_runner);
         self::assertStringContainsString('PARTNER_SYNC_ROOT_PERMISSIONS_UNSAFE', $this->cli_runner);
+        self::assertStringContainsString('PARTNER_SYNC_ROOT_GROUP_MISMATCH', $this->cli_runner);
         self::assertStringNotContainsString('/home/t/tyman5tb/', $this->cli_runner);
         self::assertStringContainsString('fn_is_development()', $this->cli_runner);
         self::assertStringContainsString('TALARIO_PARTNER_SYNC_DEV_COPY', $this->cli_runner);
@@ -190,7 +191,7 @@ final class PartnerSyncReadApiContractTest extends TestCase
         self::assertStringContainsString('trusted PHP binary owner mismatch', $this->dev_dispatcher);
         self::assertStringContainsString('trusted PHP binary is group/world writable', $this->dev_dispatcher);
         self::assertStringContainsString('mktemp "$STATE_DIR/runner.XXXXXX.php"', $this->dev_dispatcher);
-        self::assertStringContainsString('EXPECTED_RUNNER_SHA256="74b287a6940df16ed3681ef1a41560b3a44a63df168cd5817971be60b778637f"', $this->dev_dispatcher);
+        self::assertStringContainsString('EXPECTED_RUNNER_SHA256="ab41afcd39cda22a9960f7c5521eff39199ca803a0a1ad83df610a7f8b09b92d"', $this->dev_dispatcher);
         self::assertStringContainsString('partner sync CLI runner is not allowlisted', $this->dev_dispatcher);
         self::assertStringContainsString('/usr/bin/timeout --signal=TERM --kill-after=5s 60s', $this->dev_dispatcher);
         self::assertStringContainsString('/usr/bin/env -i HOME="$HOME" PATH="/usr/bin:/bin" TALARIO_PARTNER_SYNC_ROOT="$DEV_COPY"', $this->dev_dispatcher);
