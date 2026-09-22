161d6c8c958e18121da6591e444c441b88663a88
vasilpo <vasilpogonin@gmail.com>
2026-09-23 02:39:45 +0300

    fix: refresh PART-SYNC runner hash

--- ops/beget/talario-dev-github-dispatcher.sh
+++ ops/beget/talario-dev-github-dispatcher.sh
@@ -117,7 +117,7 @@ case "$REQUEST" in
     [ -x /usr/bin/git ] || fail "required git binary unavailable" 81
 
     RUNNER_REL="ops/partner-sync-apply.php"
-    EXPECTED_RUNNER_SHA256="74b287a6940df16ed3681ef1a41560b3a44a63df168cd5817971be60b778637f"
+    EXPECTED_RUNNER_SHA256="ab41afcd39cda22a9960f7c5521eff39199ca803a0a1ad83df610a7f8b09b92d"
     [ -z "$(/usr/bin/git -C "$DEV_COPY" status --porcelain --untracked-files=all)" ] || fail "dev_copy worktree must be clean for partner sync" 79
     RUNNER_COMMIT="$(/usr/bin/git -C "$DEV_COPY" rev-parse HEAD)"
     [ -n "$RUNNER_COMMIT" ] || fail "partner sync runner commit resolution failed" 80
