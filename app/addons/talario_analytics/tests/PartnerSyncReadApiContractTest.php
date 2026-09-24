d0932c78409f8774ed26ad472d9f0ada563544bc
vasilpo <vasilpogonin@gmail.com>
2026-09-24 20:16:07 +0300

    test: bound Partner Sync write diagnostic surface

--- app/addons/talario_analytics/tests/PartnerSyncReadApiContractTest.php
+++ app/addons/talario_analytics/tests/PartnerSyncReadApiContractTest.php
@@ -210,6 +210,18 @@ final class PartnerSyncReadApiContractTest extends TestCase
         self::assertStringContainsString('New Partner Sync cards are hidden by default', $this->write_capability);
     }
 
+    public function testPartnerSyncWriteFailureDiagnosticIsBoundedAndLoggingCannotMaskIt(): void
+    {
+        self::assertStringContainsString('fn_talario_analytics_partner_sync_safe_write_error_detail', $this->write_capability);
+        self::assertStringContainsString("'variation_group_create_failed'", $this->write_capability);
+        self::assertStringContainsString("'variation_product_mapping_failed'", $this->write_capability);
+        self::assertStringContainsString("'variation_product_update_failed'", $this->write_capability);
+        self::assertStringContainsString("catch (Throwable \\$log_exception)", $this->write_capability);
+        self::assertStringContainsString("\\$error_response['detail'] = \\$safe_error_detail", $this->write_capability);
+        self::assertStringNotContainsString("\\$exception->getTraceAsString()", $this->write_capability);
+    }
+
+
     public function testPartnerSyncWriteSupportsBoundedPrivateImageImport(): void
     {
         self::assertStringContainsString('content_base64', $this->write_capability);
